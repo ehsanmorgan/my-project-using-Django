e26c4f2143ee14c18706441e2d627025a9a5f6b6
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-01-03 22:24:06 +0100

    add a new files for a django project

---
+++ posts/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+from .models import post
+
+class postForm(forms.ModelForm):
+    class Meta:
+        model=post
+        #fields='__all__'
+       
+        exclude=('author',)       
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import post
+from .forms import postForm
 
 # Create your views here.
 def post_list(request):
@@ -9,3 +10,43 @@ def post_list(request):
 def post_detail(request,id):
     Post= post.objects.get(id=id)
     return render(request,'single.html',{'data':Post})
+
+
+
+def create_post(request):
+    if request.method=='POST':
+        print('in post ----')
+        form=postForm(request.POST,request.FILES)
+        if form.is_valid():
+            myform=form.save(commit=False)
+            myform.author=request.user
+            myform.save()
+
+    else:
+        print('in get ----')
+        form=postForm()
+    return render (request,'create.html',{'form':form})
+
+
+
+def edit_post(request,id):
+    Post= post.objects.get(id=id)
+    if request.method=='POST':
+        print('in post ----')
+        form=postForm(request.POST,request.FILES,instance=Post)
+        if form.is_valid():
+            myform=form.save(commit=False)
+            myform.author=request.user
+            myform.save()
+
+    else:
+
+        form=postForm(instance=Post)
+        print('in get ----')
+    return render (request,'edit.html',{'form':form})
+
+
+def delete_post(request,id):
+    Post=post.objects.get(id=id)
+    Post.delete()
+    return redirect('/blog')

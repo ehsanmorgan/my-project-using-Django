3168d145e3c8ea5eea4de4bd0db022a8eb55439e
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-01-14 19:08:23 +0100

    add

--- about/views.py
+++ about/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import About,skils
+from .models import About,skils,service
 
 
 
@@ -10,9 +10,10 @@ def create(reqeust):
     about=About.objects.last()
     skill1=skils.objects.filter(type='skill1')
     skill2=skils.objects.filter(type='skill2')
+    Service=service.objects.all()
     
 
-    return render (reqeust,'create.html',{'about':about,'skill1':skill1 , 'skill2':skill2
+    return render (reqeust,'create.html',{'about':about,'skill1':skill1 , 'skill2':skill2, 'Service':Service,
         
         
     
